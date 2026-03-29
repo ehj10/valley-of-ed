7dc38730edb840bc4f4346c2f57a32c43685a73b
Valley of Ed <ehero10@gmail.com>
2026-03-29 10:15:47 -0700

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Valley of Ed';
+export const SITE_DESCRIPTION = 'Essays on literature and philosophy, music, and the examined life.';
---
+++ src/content/blog/first-essay.md
@@ -0,0 +1,7 @@
+---
+title: 'First Essay'
+description: 'A placeholder for your first piece.'
+pubDate: 'Mar 29 2026'
+---
+
+This is where your first essay begins.
